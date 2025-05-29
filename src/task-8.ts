6032ace1d70a3875b0f98ef6656eb7b500cb2947
zadumka <90329284+zadumka@users.noreply.github.com>
2025-05-29 10:18:56 +0300

    Update task-8.ts

--- src/task-8.ts
+++ src/task-8.ts
@@ -1,18 +1,12 @@
 import axios from "axios";
 
-async function fetchPosts(): Promise<Post[]> {
-  const response = await axios.get(
+async function fetchPosts(): {
+  const response = await axios.get<Post[]>(
     "https://jsonplaceholder.typicode.com/posts"
   );
-  return response.data as string;
+  return response.data;
 }
 
 fetchPosts().then((posts) => {
   console.log(posts[0].title);
 });
-
-interface Post {
-  id: number;
-  title: string;
-  body: string;
-}
